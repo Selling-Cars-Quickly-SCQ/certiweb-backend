5e269f8620d2220a0c4a4c8eb6899df8fafa9107
JJSON-11110 <millonesjesus@outlook.com>
2025-06-12 01:25:29 -0500

    feat: configure Domain entities and Interface

---
+++ CertiWeb.API/IAM/Interfaces/REST/Resources/SignUpResource.cs
@@ -0,0 +1,2 @@
+namespace CertiWeb.API.IAM.Interfaces.REST.Resources;
+public record SignUpResource(string Username, string Password);
--- CertiWeb.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ CertiWeb.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,3 +1,4 @@
+using CertiWeb.API.IAM.Domain.Model.Entities;
 using CertiWeb.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -5,41 +6,56 @@ using Microsoft.EntityFrameworkCore;
 namespace CertiWeb.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 /// <summary>
-///     Application database context for the Learning Center Platform
+///     Application database context for the CertiWeb Platform
 /// </summary>
 /// <param name="options">
 ///     The options for the database context
 /// </param>
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-   /// <summary>
-   ///     On configuring the database context
-   /// </summary>
-   /// <remarks>
-   ///     This method is used to configure the database context.
-   ///     It also adds the created and updated date interceptor to the database context.
-   /// </remarks>
-   /// <param name="builder">
-   ///     The option builder for the database context
-   /// </param>
-   protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    /// <summary>
+    /// Users DbSet for authentication
+    /// </summary>
+    public DbSet<User> Users { get; set; }
+
+    /// <summary>
+    ///     On configuring the database context
+    /// </summary>
+    /// <remarks>
+    ///     This method is used to configure the database context.
+    ///     It also adds the created and updated date interceptor to the database context.
+    /// </remarks>
+    /// <param name="builder">
+    ///     The option builder for the database context
+    /// </param>
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         builder.AddCreatedUpdatedInterceptor();
         base.OnConfiguring(builder);
     }
 
-   /// <summary>
-   ///     On creating the database model
-   /// </summary>
-   /// <remarks>
-   ///     This method is used to create the database model for the application.
-   /// </remarks>
-   /// <param name="builder">
-   ///     The model builder for the database context
-   /// </param>
-   protected override void OnModelCreating(ModelBuilder builder)
+    /// <summary>
+    ///     On creating the database model
+    /// </summary>
+    /// <remarks>
+    ///     This method is used to create the database model for the application.
+    /// </remarks>
+    /// <param name="builder">
+    ///     The model builder for the database context
+    /// </param>
+    protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        // Configure User entity
+        builder.Entity<User>(entity =>
+        {
+            entity.HasKey(u => u.Id);
+            entity.Property(u => u.Name).IsRequired().HasMaxLength(100);
+            entity.Property(u => u.Email).IsRequired().HasMaxLength(255);
+            entity.Property(u => u.PasswordHash).IsRequired();
+            entity.HasIndex(u => u.Email).IsUnique();
+        });
 
         builder.UseSnakeCaseNamingConvention();
     }
